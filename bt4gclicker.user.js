1f9664005c8b9b84b29cf5c0e528f6a6120c85c6
kolakao <pozdro600lol900@o2.pl>
2022-12-11 13:54:49 +0100

    add downloadURL, updateURL

--- bt4gclicker.user.js
+++ bt4gclicker.user.js
@@ -8,6 +8,8 @@
 // @match        http://bt4g.org/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
+// @updateURL    https://github.com/kolakao/bt4g-clicker/blob/master/bt4gclicker.meta.js
+// @downloadURL  https://github.com/kolakao/bt4g-clicker/blob/master/bt4gclicker.user.js
 // ==/UserScript==
 
 (function() {
